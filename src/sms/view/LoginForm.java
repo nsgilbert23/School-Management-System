3c7ba8e46bd01419dceb2233954b8322010ff5b9
BelyseU <umurerwabel@gmail.com>
2026-03-09 20:56:30 +0200

    Added Login page with input validation

--- src/sms/view/LoginForm.java
+++ src/sms/view/LoginForm.java
@@ -1,4 +1,4 @@
-
+// Login page with username and password validation - BelyseU
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
